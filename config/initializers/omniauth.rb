89e470991f59aa094e589e157536d586a51c7889
Bob Roberts <broberts_az@yahoo.com>
2012-03-14 19:04:22 -0700

    removed provider key need

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,19 +1,19 @@
-provider_keys = YAML.load_file(File.join(Rails.root, 'config', 'provider_keys.yml'))
+# provider_keys = YAML.load_file(File.join(Rails.root, 'config', 'provider_keys.yml'))
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider(
-    :facebook,
-    provider_keys[Rails.env]['facebook']['consumer_key'],
-    provider_keys[Rails.env]['facebook']['consumer_secret'],
-    display: 'popup',
-    scope: 'offline_access'
-  )
-
-  provider(
-    :twitter,
-    provider_keys[Rails.env]['twitter']['consumer_key'],
-    provider_keys[Rails.env]['twitter']['consumer_secret']
-  )
+  # provider(
+  #   :facebook,
+  #   provider_keys[Rails.env]['facebook']['consumer_key'],
+  #   provider_keys[Rails.env]['facebook']['consumer_secret'],
+  #   display: 'popup',
+  #   scope: 'offline_access'
+  # )
+  # 
+  # provider(
+  #   :twitter,
+  #   provider_keys[Rails.env]['twitter']['consumer_key'],
+  #   provider_keys[Rails.env]['twitter']['consumer_secret']
+  # )
 
 
   provider :identity, model: User,
